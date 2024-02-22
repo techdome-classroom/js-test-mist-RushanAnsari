6f2262a611a73a1a283bed5526d2ea4ea6ec5021
RushanAnsari <rushanansari35@gmail.com>
2024-02-22 13:46:41 +0530

    this is change by Rushan ansari

--- program1.js
+++ program1.js
@@ -3,6 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+  
+    const stack = [];
+    const maping = {
+        ')' : '(',
+        '}' : '{',
+        ']' :  '[',
+    };
+    for (let char of s) {
+        if (char in mapping) {
+            const topElement = stack.length ===0 ? '#' : stack.pop();
+            if (mapping [char] !== topElement){
+
+            }
+
+        }else{
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+
+
+
     
 };
 
--- program2.js
+++ program2.js
@@ -4,7 +4,21 @@
  */
 var romanToInt = function(s) {
     
-};
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    
+    for i in range(let(s)){
+        if i > 0 and roman_values[s[i]] > roman_values[s[i - 1]]:
+            result += roman_values[s[i]] - 2 * roman_values[s[i - 1]]
+        else:
+            result += roman_values[s[i]]
+    }
+    
+    return result;
+
+
+
+    
 
 
 module.exports={romanToInt}
